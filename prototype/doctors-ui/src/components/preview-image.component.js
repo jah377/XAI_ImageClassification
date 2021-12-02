60ce313f54cf3731b64b4d50c8e3f7c38314499c
Jon Harris <jonathan.harris.eit@gmail.com>
2021-12-02 13:19:05 +0100

    Add zoom-pan to PreviewImages

--- prototype/doctors-ui/src/components/preview-image.component.js
+++ prototype/doctors-ui/src/components/preview-image.component.js
@@ -1,13 +1,19 @@
 import React, { useContext } from "react";
 import { StepContext } from "../context/StepContext";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { render } from "@testing-library/react";
 
 
 export default function PreviewImages(props) {
 
     const [context] = useContext(StepContext);
     console.log(context)
-
+    render()
     return (
-        <img className="preview" src={context.previewImage} alt="" />
+        <TransformWrapper>
+            <TransformComponent>
+                <img className="preview" src={context.previewImage} alt="" />
+            </TransformComponent>
+        </TransformWrapper>
     );
 }
