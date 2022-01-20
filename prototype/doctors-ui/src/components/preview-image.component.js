6be1d0a87036883a6f0d35c59d413f9655e3f1b2
jah377 <jonathan.harris.eit@gmail.com>
2022-01-20 11:20:08 +0100

    preview - add next page button

--- prototype/doctors-ui/src/components/preview-image.component.js
+++ prototype/doctors-ui/src/components/preview-image.component.js
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { StepContext } from "../context/StepContext";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { Button } from "@mui/material";
 import Stack from '@mui/material/Stack';
 import PreviewImagesUserNotes from "./preview-notes.component";
 
 export default function PreviewImages(props) {
 
-    const [context] = useContext(StepContext);
+    const [context, setContext] = useContext(StepContext);
 
     return (
         <div>
@@ -22,7 +23,15 @@ export default function PreviewImages(props) {
                     </TransformComponent>
                 </TransformWrapper>
 
-                <PreviewImagesUserNotes />
+                {/* right column */}
+                <Stack direction="column" alignItems="flex-start" justifyContent="center" spacing={2}>
+                    <PreviewImagesUserNotes />
+
+                    <Button fullWidth variant="contained" onClick={() => setContext(context => ({
+                        ...context,
+                        step: context.step + 1, // incrementing the step counter, we get navigated to the next step "Preview Image"
+                    }))} >Go To Analysis</Button>
+                </Stack>
             </Stack>
         </div>
     );
